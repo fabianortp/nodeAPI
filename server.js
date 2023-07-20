a594960778d5d9c7242a7dae5e71678e47d31d34
Fabiano Pereira <fabianorodrigues912@gmail.com>
2023-07-20 06:13:28 -0300

    Add nodemon to the project

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.send('Hello NODE API')
 })
 
+app.get('/blog', (req, res) => {
+    res.send('Hello blog')
+})
+
 app.listen(3000, ()=> {
     console.log(`Node API app is running on port 3000`)
 })

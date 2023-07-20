1121004b79fcdf6b5d529d3d6bdd994d4cb50584
Fabiano Pereira <fabianorodrigues912@gmail.com>
2023-07-20 06:44:47 -0300

    Add moongose package to conect node to mongoDB

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 app.get('/', (req, res) => {
@@ -6,9 +7,21 @@ app.get('/', (req, res) => {
 })
 
 app.get('/blog', (req, res) => {
-    res.send('Hello blog')
+    res.send('Hello blog, My name is Fabiano')
 })
 
+/*Moved to mongoose.connect
 app.listen(3000, ()=> {
     console.log(`Node API app is running on port 3000`)
+})*/
+
+mongoose.set("strictQuery", false)
+mongoose.connect('mongodb+srv://admin:admin@fabianoapi.k47xvj4.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(3000, () => {
+        console.log(`Node API app is running on port 3000`)
+    });
+}).catch((error) => {
+    console.log(error)
 })
